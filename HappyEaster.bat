b009cd4d7314c97e0bd198d62b44fcc538708a58
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2022-04-23 15:13:39 +0200

    Add files via upload

---
+++ HappyEaster.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Happy Easter!                             -Ber Ber
+echo Fun Fact: White, gold and purple are the main colors of Easter.
+:colorloop
+timeout 1 /nobreak >nul
+color 57
+timeout 1 /nobreak >nul
+color 70
+timeout 1 /nobreak >nul
+color 6f
+goto colorloop
